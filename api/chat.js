045c0be32ba5938ccb0a7787d287af6856addef5
ClaudioR2062 <jtb2s98xk2@privaterelay.appleid.com>
2026-05-09 23:22:16 +0200

    Add files via upload

--- api/chat.js
+++ api/chat.js
@@ -66,7 +66,7 @@ ${message}
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          model: "gpt-5.3",
+          model: "gpt-4.1-mini",
           input: prompt
         })
       }
